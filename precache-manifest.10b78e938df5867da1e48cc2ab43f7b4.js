e9f1fad2db180a8240fc1cb2d27edb6f2404e607
Subhash Yadav <subhash@websultanate.com>
2022-08-25 16:10:48 +0530

    first commit

---
+++ precache-manifest.10b78e938df5867da1e48cc2ab43f7b4.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9ad7cbbab13d49596009d481b7d6699c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a44b4b59fa8928915ac4",
+    "url": "/static/css/main.72efa7d9.chunk.css"
+  },
+  {
+    "revision": "967b5fe61e4a214c0af4",
+    "url": "/static/js/2.58d0e7fb.chunk.js"
+  },
+  {
+    "revision": "88cbcb55f867c7e879dbc5e47becbf3b",
+    "url": "/static/js/2.58d0e7fb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a44b4b59fa8928915ac4",
+    "url": "/static/js/main.2766c4a5.chunk.js"
+  },
+  {
+    "revision": "6eddee88f3050a0f8645",
+    "url": "/static/js/runtime-main.5d2bcaa6.js"
+  },
+  {
+    "revision": "f565cc2d8a408e5175953b9799569684",
+    "url": "/static/media/logo.f565cc2d.png"
+  },
+  {
+    "revision": "d2a5b53409e1db3c0c183dbd46531511",
+    "url": "/static/media/pattern.d2a5b534.jpg"
+  }
+]);
